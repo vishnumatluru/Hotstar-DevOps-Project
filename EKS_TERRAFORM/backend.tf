35278b3460ae2ce7b262af24a78b3bfeb16159f4
vishnumatluru <121228637+vishnumatluru@users.noreply.github.com>
2024-02-08 13:49:54 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "vishnuproject143-hostar" # Replace with your actual S3 bucket name
+    bucket = "vishnutetrisv1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "eu-west-3"
+    region = "eu-west-2"
   }
 }
