ba862006e39b31fec820c04854bcc098981f00ae
vishnumatluru <121228637+vishnumatluru@users.noreply.github.com>
2024-02-13 18:53:37 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "vishnutetrisv1" # Replace with your actual S3 bucket name
+    bucket = "vishnunetflix" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "eu-west-2"
   }
